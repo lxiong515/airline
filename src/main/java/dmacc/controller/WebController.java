aaba440c2b7e00085e226e09ac253d814048ea7c
lxiong515 <58718122+lxiong515@users.noreply.github.com>
2020-11-14 09:12:23 -0600

    Added input.html and started WebController
    Can't complete WebController yet. Need repo and to add a method

---
+++ src/main/java/dmacc/controller/WebController.java
@@ -0,0 +1,12 @@
+package dmacc.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebController {
+	//@Autowired
+	//list repo here;
+	
+	//@GetMapping("/employee")
+	//enter method here
+}
