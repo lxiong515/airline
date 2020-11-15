41cfea4b03c2ce9a887ca02ce0c4ad5581969bfe
lxiong515 <58718122+lxiong515@users.noreply.github.com>
2020-11-15 11:37:57 -0600

    mapped the html files
    index for landing. maps to employee or customer view. customer view leads to input view

--- src/main/java/dmacc/beans/CustomerReservation.java
+++ src/main/java/dmacc/beans/CustomerReservation.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+//@Embeddable - would have to autowire to Customer
 @Entity
 public class CustomerReservation {
 	@Id
--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -1,15 +1,63 @@
 package dmacc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import dmacc.beans.Customer;
+import dmacc.beans.CustomerReservation;
+import dmacc.beans.Flight;
+import dmacc.repository.CustomerRepository;
 
 @Controller
 public class WebController {
+	//added the mapping below to get to the employee & customer links
+	@Autowired
+	CustomerRepository custRepo;
 	
-	// I am thinking not needed because we will have table specific repositories
+	@GetMapping("/viewAll")
+	public String viewAllCustomers(Model model) {
+		model.addAttribute("customers",custRepo.findAll());
+		return "results";
+	}
+	@GetMapping("/input")
+	public String addNewCustomer(Model model) {
+		Customer c = new Customer();
+		model.addAttribute("newCustomer", c);
+		return "input";
+	}
+	@PostMapping("/input")
+	public String addNewCustomer(@ModelAttribute Customer c, Model model) {
+		custRepo.save(c);
+		return viewAllCustomers(model);
+	}
+	@GetMapping("/employee")
+	public String viewEmployee(Model model) {
+		//added all the classes just in case
+		//feel free to change info below
+		Customer c = new Customer();
+		model.addAttribute("newCustomer", c);
+		CustomerReservation cr = new CustomerReservation();
+		model.addAttribute("newReservation", cr);
+		Flight f = new Flight();
+		model.addAttribute("newFlight", f);
+		return "employee";
+	}
+	@GetMapping("/customer")
+	public String viewCustomer(Model model) {
+		//added all the classes just in case
+				//feel free to change info below
+		Customer c = new Customer();
+		model.addAttribute("newCustomer", c);
+		CustomerReservation cr = new CustomerReservation();
+		model.addAttribute("newReservation", cr);
+		Flight f = new Flight();
+		model.addAttribute("newFlight", f);
+		return "customer";
+	}
+		
 	
-	//@Autowired
-	//list repo here;
-	
-	//@GetMapping("/employee")
-	//enter method here
 }
