0105398e317fba9420ddeb0aafa50e72ea9c793e
rmcggee <ramcgee1@dmacc.edu>
2020-11-14 21:28:25 -0600

    Created classes and ran as spring boot app writing data to db from repos

---
+++ src/main/java/dmacc/beans/Flight.java
@@ -0,0 +1,75 @@
+package dmacc.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Flight {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String departCity;
+	private String destinCity;
+	private int capacity;
+	private double seatCostAmt;
+	
+	public Flight() {
+	}
+
+	public Flight(String departCity, String destinCity, int capacity, double seatCostAmt) {
+		super();
+		//this.id = id;
+		this.departCity = departCity;
+		this.destinCity = destinCity;
+		this.capacity = capacity;
+		this.seatCostAmt = seatCostAmt;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+
+	public String getDepartCity() {
+		return departCity;
+	}
+
+	public void setDepartCity(String departCity) {
+		this.departCity = departCity;
+	}
+
+	public String getDestinCity() {
+		return destinCity;
+	}
+
+	public void setDestinCity(String destinCity) {
+		this.destinCity = destinCity;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public double getSeatCostAmt() {
+		return seatCostAmt;
+	}
+
+	public void setSeatCostAmt(double seatCostAmt) {
+		this.seatCostAmt = seatCostAmt;
+	}
+
+	@Override
+	public String toString() {
+		return "Flight [id=" + id + ", departCity=" + departCity + ", destinCity=" + destinCity + ", capacity="
+				+ capacity + ", seatCostAmt=" + seatCostAmt + "]";
+	}
+
+	
+
+}
---
+++ src/main/java/dmacc/controller/FlightController.java
@@ -0,0 +1,20 @@
+package dmacc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import dmacc.beans.Flight;
+import dmacc.repository.FlightRepository;
+
+@Controller
+public class FlightController {
+	
+	@Autowired
+	FlightRepository flightRepo;
+	
+}
