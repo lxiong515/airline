c3692346925b2f55793906bef041281f786f2e18
lxiong515 <58718122+lxiong515@users.noreply.github.com>
2020-11-16 00:51:37 -0600

    Added Delete in Web Controller - no "results" page to add html link yet

--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import dmacc.beans.Customer;
@@ -57,6 +58,12 @@ public class WebController {
 		Flight f = new Flight();
 		model.addAttribute("newFlight", f);
 		return "customer";
+	}
+	@GetMapping("/delete/{id}")
+	public String deleteCustomer(@PathVariable("id") long id, Model model) {
+		Customer c = custRepo.findById(id).orElse(null);
+		custRepo.delete(c);
+		return viewAllCustomers(model);
 	}
 		
 	
