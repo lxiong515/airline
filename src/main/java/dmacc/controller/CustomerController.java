ddad9ec4a8ba295c94ace57ebaeaf14e3611b28f
Walkingdeadj <jshi@dmacc.edu>
2020-11-25 11:18:57 -0600

    Partial work done

--- src/main/java/dmacc/controller/CustomerController.java
+++ src/main/java/dmacc/controller/CustomerController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import dmacc.beans.Customer;
-import dmacc.beans.CustomerReservation;
-import dmacc.beans.Flight;
 import dmacc.repository.CustomerRepository;
 
 @Controller
@@ -18,35 +16,39 @@ public class CustomerController {
 	@Autowired
 	CustomerRepository custRepo;
 	//added this below to see if it works outside of webcontroller
-	@GetMapping("/customer")
+	@GetMapping("/customerList")
 	public String viewCustomer(Model model) {
-		//added all the classes just in case
-				//feel free to change info below
-		Customer c = new Customer();
-		model.addAttribute("newCustomer", c);
-		CustomerReservation cr = new CustomerReservation();
-		model.addAttribute("newReservation", cr);
-		Flight f = new Flight();
-		model.addAttribute("newFlight", f);
-		return "customer";
+		if(custRepo.findAll().isEmpty()) {
+			return addNewCustomer(model);
+		}
+		model.addAttribute("customers", custRepo.findAll());
+		return "customerList";
 	}
-	/*method viewAllCustomers doesn't work in this controller
-	@PostMapping("/input")
-	public String addNewCustomer(@ModelAttribute Customer c, Model model) {
-		custRepo.save(c);
-		return viewAllCustomers(model);
-	}
-	@GetMapping("/delete/{id}")
-	public String deleteCustomer(@PathVariable("id") long id, Model model) {
-		Customer c = custRepo.findById(id).orElse(null);
-		custRepo.delete(c);
-		return viewAllCustomers(model);
-	}*/
+	
 	@GetMapping("/input")
 	public String addNewCustomer(Model model) {
 		Customer c = new Customer();
 		model.addAttribute("newCustomer", c);
 		return "input";
+	}
+	//method viewAllCustomers doesn't work in this controller
+	@PostMapping("/input")
+	public String addNewCustomer(@ModelAttribute Customer c, Model model) {
+		custRepo.save(c);
+		return viewCustomer(model);
+	}
+	
+	@GetMapping("/deleteCustomer/{id}")
+	public String deleteCustomer(@PathVariable("id") long id, Model model) {
+		Customer c = custRepo.findById(id).orElse(null);
+		custRepo.delete(c);
+		return viewCustomer(model);
+	}
+	
+	@GetMapping("/updateCustomer/{id}")
+	public String reviseCustomer(Customer c, Model model) {
+		custRepo.save(c);
+		return viewCustomer(model);
 	}
 	
 }
