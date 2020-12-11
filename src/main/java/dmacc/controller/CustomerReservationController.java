bd155e8f37ee76ac6fbb1bfbcdabe78a67267db5
Rob Nervig <ranervig@gmail.com>
2020-12-10 23:02:31 -0600

    added seat picker functionality and update functionality

--- src/main/java/dmacc/controller/CustomerReservationController.java
+++ src/main/java/dmacc/controller/CustomerReservationController.java
@@ -1,6 +1,6 @@
 package dmacc.controller;
 
-import java.io.Console;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,11 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import dmacc.beans.Customer;
 import dmacc.beans.CustomerReservation;
-import dmacc.beans.Flight;
+
 import dmacc.repository.CustomerRepository;
 import dmacc.repository.CustomerReservationRepository;
 import dmacc.repository.FlightRepository;
@@ -27,17 +25,17 @@ public class CustomerReservationController {
 	CustomerRepository custRepo;
 	@Autowired
 	FlightRepository flightRepo;
-	
+
 	@GetMapping("/reservation")
 	public String viewAllReservations(Model model) {
-		if(crRepo.findAll().isEmpty()) {
+		if (crRepo.findAll().isEmpty()) {
 			return addNewReservation(model);
 		}
 		model.addAttribute("reservations", crRepo.findAll());
 		return "reservation";
 	}
-	
-	@GetMapping("/inputRes")
+
+	@GetMapping("/inputReservation")
 	public String addNewReservation(Model model) {
 		CustomerReservation cres = new CustomerReservation();
 		model.addAttribute("newReservation", cres);
@@ -45,31 +43,38 @@ public class CustomerReservationController {
 		model.addAttribute("customers", custRepo.findAll());
 		return "inputRes";
 	}
-	
-	@PostMapping("/inputReservation")
-	public String addReservation(Model model, @RequestParam String custId, @RequestParam String flightId, @RequestParam String seatId) {
-		long tempCustId = Long.parseLong(custId);
-		long tempFlightId = Long.parseLong(flightId);
-		Customer myCust = custRepo.findById(tempCustId).orElse(null);
-		Flight myFlight = flightRepo.findById(tempFlightId).orElse(null);
-		CustomerReservation cres = new CustomerReservation(seatId, myFlight, myCust );
-		crRepo.save(cres);
-		return viewAllReservations(model);
+
+	@PostMapping("/inputReservation/{id}")
+	public String addReservation(@ModelAttribute CustomerReservation cres, @PathVariable("id") long id, Model model) {
+		List<CustomerReservation> reservations = crRepo.findByFlight_Id(cres.getFlight().getId());
+		String[] seatsTaken = new String[reservations.size()];
+		for (int i = 0; i < reservations.size(); i++) {
+			seatsTaken[i] = reservations.get(i).getSeatIdentifierText();
+		}
+		System.out.println(id);
+		model.addAttribute("seatsTaken", seatsTaken);
+		model.addAttribute("newReservation", cres);
+		model.addAttribute("id", id);
+		return "/pickSeat";
 	}
 
 	@GetMapping("/editReservation/{id}")
 	public String showUpdateReservation(@PathVariable("id") long id, Model model) {
-		CustomerReservation cres = new CustomerReservation();
+		CustomerReservation cres = crRepo.findById(id).orElse(null);
 		model.addAttribute("newReservation", cres);
+		model.addAttribute("flights", flightRepo.findAll());
+		model.addAttribute("customers", custRepo.findAll());
 		return "inputRes";
 	}
-	
+
 	@PostMapping("/updateReservation/{id}")
-	public String reviseReservation(CustomerReservation cres, Model model) {
+	public String reviseReservation(CustomerReservation cres, @PathVariable("id") long id, Model model) {
+		//to avoid the bean reseting the id when it is passed
+		cres.setId(id);
 		crRepo.save(cres);
 		return viewAllReservations(model);
 	}
-	
+
 	@GetMapping("/deleteReservation/{id}")
 	public String deleteUser(@PathVariable("id") long id, Model model) {
 		CustomerReservation cres = crRepo.findById(id).orElse(null);
@@ -77,5 +82,4 @@ public class CustomerReservationController {
 		return viewAllReservations(model);
 	}
 
-	
 }
